b078776fad7173f38f91c56590f8b467b7bf39b2
Yauheni Bandziukevich <yauheni.bandziukevich@innowise-group.com>
2024-04-16 19:14:11 +0200

    feat: add server pagination

--- srv/src/users/users.service.ts
+++ srv/src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersEntity } from './users.entity';
 import { Repository } from 'typeorm';
+import { PaginationDto } from './pagination.dto';
 
 @Injectable()
 export class UserService {
@@ -13,7 +14,12 @@ export class UserService {
   ) {}
 
   // get list of all users
-  async findAll(): Promise<UsersEntity[]> {
-    return await this.usersRepo.find();
+  async findAll({ page, limit }: PaginationDto): Promise<UsersEntity[]> {
+    const skippedItems = (page - 1) * limit;
+
+    return await this.usersRepo.find({
+      skip: skippedItems,
+      take: limit,
+    });
   }
 }
